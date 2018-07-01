8d81732cdcb46acd842457edb8146855c169bfe8
Julia <juliafsfalcao@hotmail.com>
2018-07-01 13:21:42 -0300

    ajustando as coisas

--- simulator.py
+++ simulator.py
@@ -1,33 +1,38 @@
 from generators import *
 
-ARRIVAL = 'A'
-DEPARTURE = 'D'
+# event types
+ARRIVAL = 'A' # client arrives at the restaurant
+# ENTRANCE = 'E' # client goes inside / is served
+DEPARTURE = 'D' # client leaves the restaurant
 
-# EVENT: [event_type, event_time]
+# EVENT: (event_type, event_time)
 EVENT_TYPE = 0
 EVENT_TIME = 1
 
-def get_event_time(event):
-    return event[1]
-
-
-def simulator():
+"""
+Main simulator function.
+restaurant_capacity: how many people can be served at the same time
+queue_capacity: how many people can wait in line at the same time
+arrival_rate: average number of clients that arrive per time unit (lambda)
+"""
+def restaurant_simulator(restaurant_capacity = 30, queue_capacity = 15, arrival_rate = 0.2):
     current_time = 0
-    max_time = 10000 # Simulation end condition
-    events = []
+    max_time = 1 # simulation end condition
+    events = [] # list of (event_type, event_time) tuples, sorted by event time
+    serving = 0 # amount of people currently being served (serving <= restaurant_capacity)
+    queue_size = 0 # amount of people currently waiting in line (queue_size <= queue_capacity)
 
-    # Interest measures
-    arrival_count = 0 # total of arrivals
-    departure_count = 0 # total of departures
-    queue_size = 0 # current queue size
-    queue_capacity = 15 # maximum queue size acceptable before starting to drop events
-    drop_count = 0 # amount of dropped events
+    # interest measures
+    arrival_count = 0
+    departure_count = 0
+    drop_count = 0 # amount of events dropped due to queue being full
 
     arrival_times = []
+    entrance_times = []
     departure_times = []
 
     # Bootstrap
-    events.append([ARRIVAL, current_time])
+    events.append((ARRIVAL, current_time))
 
     # Simulator loop
     while len(events) > 0 and current_time < max_time:
@@ -35,46 +40,48 @@ def simulator():
 
         current_state = events.pop(0)
 
+        # process arrival
         if current_state[EVENT_TYPE] == ARRIVAL:
             current_time = current_state[EVENT_TIME]
             arrival_count += 1
 
-            if queue_size == queue_capacity:
-                # Queue full -> drop event
-                drop_count += 1
-
-            else:
-                queue_size += 1
-                print(f"queue size: {queue_size}")
+            if serving < restaurant_capacity: # restaurant not full -> client is served
+                serving += 1
                 arrival_times.append(current_time)
+                entrance_times.append(current_time) # client arrives and is served at the same time
 
-            # Time until next arrival
-            seed = time.clock() * pow(10, 20)
-            next_arrival = int(linear_congruential_generator(seed)[0] * 100)
-            events.append([ARRIVAL, current_time + next_arrival])
-            events = sorted(events, key = lambda x: x[EVENT_TIME])
+                service_duration = exponential_generator(time.time()) # random variable X
+                events.append((DEPARTURE, current_time + service_duration))
+                events = sorted(events, key = lambda e: e[EVENT_TIME])
 
-            if queue_size == 1:  # Queue was empty
-                # Time until next departure
-                seed = time.clock() * pow(10, 20)
-                next_departure = int(linear_congruential_generator(seed)[0] * 100)
-                events.append([DEPARTURE, current_time + next_departure])
-                events = sorted(events, key = lambda x: x[EVENT_TIME])
+            else: # restaurant full -> check if client can wait in line
+                if queue_size < queue_capacity: # queue not full -> client joins line
+                    queue_size += 1
+                    arrival_times.append(current_time)
 
+                else: # queue full -> client leaves
+                    drop_count += 1
 
+        # process departure
         elif current_state[EVENT_TYPE] == DEPARTURE:
             current_time = current_state[EVENT_TIME]
-            queue_size -= 1
-            print(f"queue size: {queue_size}")
+            serving -= 1
             departure_count += 1
 
-            departure_times.append(current_time)
+            if queue_size > 0: # line is not empty -> next client is served
+                queue_size -= 1
+                serving += 1
 
-            if queue_size > 0:  # Queue is not empty
-                # Time until next departure
-                seed = time.clock() * pow(10, 20)
-                next_departure = int(linear_congruential_generator(seed)[0] * 100)
-                events.append([DEPARTURE, current_time + next_departure])
+                service_duration = exponential_generator(time.time()) # random variable X
+                events.append((DEPARTURE, current_time + service_duration))
+                events = sorted(events, key = lambda e: e[EVENT_TIME]) 
+
+        # add next arrival
+        time_until_next_arrival = exponential_generator(time.time())
+        events.append((ARRIVAL, current_time + time_until_next_arrival))
+        events = sorted(events, key = lambda e: e[EVENT_TIME])
+
+
 
     # Measures
     drop_rate = drop_count / arrival_count
@@ -95,4 +102,4 @@ def simulator():
     # print(f"average waiting time: {avg_waiting_time}")
 
 
-simulator()
+restaurant_simulator()
